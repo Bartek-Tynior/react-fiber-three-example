c52072f31cfc648e5e569d8fc355dfd020c1c12e
bart tynior <btynior@gmail.com>
2024-07-17 23:35:26 +0200

    Initial commit of floating shapes page

---
+++ src/app/components/FloatingShapes.tsx
@@ -0,0 +1,7 @@
+export default function FloatingShapes() {
+    return (
+        <div>
+            Floating Shapes placeholder
+        </div>
+    )
+}
---
+++ src/app/floating-shapes/page.tsx
@@ -0,0 +1,9 @@
+import FloatingShapes from "../components/FloatingShapes";
+
+export default function Page() {
+    return (
+        <main>
+            <FloatingShapes />
+        </main>
+    )
+}
